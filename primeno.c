de7a00d33b6771be9374b52826f55389e541bfab
Niranjana12 <niranjanaprabhu67@gmail.com>
2016-06-25 11:12:36 +0530

    Update primeno.c

--- primeno.c
+++ primeno.c
@@ -2,15 +2,18 @@
 int main()
 {
 	int n;
+	int flag=0;
 	printf("\nEnter a number");
 	scanf("%d",&n);
+	if(n==1)
+ 	printf("Neither prime nor composite"); 
 	for(i=2;i<=n-1;i++)
 	  {
 	    if(n%i==0)
    	      flag=flag+1;
 	  }
 	if(flag==0)
-	   printf("\nPrime number"0;
+	   printf("\nPrime number");
 	else
 	   printf("\nNot a Prime Number");
 	return 0;
